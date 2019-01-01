ddabc5b12aade4dfc12c730277215ea4e48e4a04
spencerchan <spencerchan@uchicago.edu>
2018-12-31 19:15:48 -0600

    update theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Spencer Chan'
 SITENAME = 'Spencer Chan'
-SITEURL = ''
+SITEURL = 'https://spencerchan.github.io/data-blog'
 
 PATH = 'content'
 
@@ -19,17 +19,17 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+GITHUB_URL = 'http://github.com/spencerchan'
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
+'''LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+         ('Jinja2', 'http://jinja.pocoo.org/'),)'''
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
+SOCIAL = (('github', GITHUB_URL),)
 DEFAULT_PAGINATION = 10
+
+TAGLINE = "aspiring data scientist / pythonista / malört caucus"
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
@@ -39,4 +39,11 @@ MARKUP = ('md', 'ipynb')
 PLUGIN_PATHS = ['./plugins']
 PLUGINS = ['ipynb.markup']
 
-OUTPUT_PATH = 'docs'
+IPYNB_SKIP_CSS=True
+THEME = 'themes/pelican-hss'
+
+STATIC_PATHS = ['extra']
+CUSTOM_CSS_URL = '/static/custom.css'
+EXTRA_PATH_METADATA = {
+	'extra/custom.css': {'path': 'static/custom.css'},
+}
